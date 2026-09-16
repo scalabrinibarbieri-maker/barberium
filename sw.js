e25b9260ecd8290f96d9d73342be5e9c113e213f
scalabrinibarbieri-maker <scalabrinibarbieri@gmail.com>
2026-09-16 09:23:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='scalabrini-client-v10';
+const CACHE='scalabrini-client-v11';
 const PRECACHE=[
-  './','./index.html','./styles.css?v=7','./app.js?v=7','./site.webmanifest','./equipe/','./equipe/index.html','./equipe/team.css?v=10','./equipe/team.js?v=10',
+  './','./index.html','./styles.css?v=7','./app.js?v=7','./membership.css?v=11','./membership.js?v=11','./site.webmanifest','./equipe/','./equipe/index.html','./equipe/team.css?v=11','./equipe/team.js?v=11',
   './assets/corte-barba-sobrancelha.webp','./assets/combo-corte-barba.webp','./assets/combo-corte-barboterapia.webp',
   './assets/barba-express.webp','./assets/barba-tradizionale.webp','./assets/pezinho-detalhes.webp','./assets/sobrancelha.webp',
   './assets/vinicius-nunes.webp','./assets/barboterapia.webp','./assets/logo-sb.webp','./assets/cabeca-raspada.webp',
