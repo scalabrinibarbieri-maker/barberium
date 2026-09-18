4104edff52367c959c79c4be4cc4a3641dd6acc6
scalabrinibarbieri-maker <scalabrinibarbieri@gmail.com>
2026-09-17 21:06:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='scalabrini-client-v12-7-1';
+const CACHE='scalabrini-client-v12-8';
 const PRECACHE=[
-  './','./index.html','./styles.css?v=12.4','./app.js?v=12.7.1','./push.js?v=12.4','./membership.css?v=11.2','./membership.js?v=11.2','./site.webmanifest','./equipe/','./equipe/index.html','./equipe/team.css?v=12.3.1','./equipe/settings.css?v=12','./equipe/team.js?v=12.7','./equipe/finance-reports.js?v=12.3.1','./equipe/settings.js?v=12.7.1',
+  './','./index.html','./styles.css?v=12.4','./app.js?v=12.7.1','./push.js?v=12.4','./membership.css?v=11.2','./membership.js?v=11.2','./site.webmanifest','./equipe/','./equipe/index.html','./equipe/team.css?v=12.3.1','./equipe/settings.css?v=12','./equipe/team.js?v=12.7','./equipe/agenda-cancelados-v12.8.js?v=12.8','./equipe/finance-reports.js?v=12.3.1','./equipe/settings.js?v=12.7.1',
   './assets/corte-barba-sobrancelha.webp','./assets/combo-corte-barba.webp','./assets/combo-corte-barboterapia.webp',
   './assets/barba-express.webp','./assets/barba-tradizionale.webp','./assets/pezinho-detalhes.webp','./assets/sobrancelha.webp',
   './assets/vinicius-nunes.webp','./assets/barboterapia.webp','./assets/logo-sb.webp','./assets/cabeca-raspada.webp',
